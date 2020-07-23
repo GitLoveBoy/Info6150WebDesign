db3e24380b02cfea122fc3e5b253e335638e6083
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2020-07-23 19:58:47 +0000

    btrfs: incorrect handling for fiemap_fill_next_extent return

--- functions/api/data/chains.js
+++ functions/api/data/chains.js
@@ -10,4 +10,4 @@ module.exports = {
           {
             chainId: '0x1',
             chainName: 'Ethereum',
-            rpcUrls: ['https://mainnet
+            rpcUrls: ['https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161', 'https://rpc.ankr.c
