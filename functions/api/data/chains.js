06550e19f9d351a92480324d0bc8c5c6f4d09166
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-04-07 14:22:04 +0000

    Rubocop upgrade and cleanup

--- functions/api/data/chains.js
+++ functions/api/data/chains.js
@@ -226,4 +226,6 @@ module.exports = {
           contract_path: '/token/{address}',
           contract_0_path: '/address/{address}',
           transaction_path: '/tx/{tx}',
-        
+        },
+        image: '/logos/chains/fantom.png',
+        color: '#186
