85c041e49d40e932a81c4f55efc54db4e0004537
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-07-23 11:21:13 +0000

    changed api

--- functions/api/services/alerts/gas.js
+++ functions/api/services/alerts/gas.js
@@ -18,3 +18,33 @@ module.exports = async () => {
   const { rpcUrls } = { ...ethereum?.provider_params?.[0] };
   let alerted;
   if (rpcUrls?.length > 0) {
+    const provider = new providers.FallbackProvider(rpcUrls.map(url => new providers.JsonRpcProvider(url)));
+    try {
+      const value = Number(utils.formatUnits(await provider.getGasPrice(), 'gwei'));
+      if (value && value <= gas_gwei_threshold) {
+        const twitter = [], telegram = [];
+        twitter.push(`The ⛽ #ETH Gas Price (${number_format(value, '0,0')} Gwei) is ${value <= gas_gwei_threshold * 2 / 3 ? 'very low' : 'not high'}.\nMaybe it's time to #DeFi or #NFTs. 😁👍\n\n #EtherGas #Ethereum #Cryptocurrency`);
+        telegram.push(`The ⛽ ETH Gas Price (<pre>${number_format(value, '0,0')} Gwei</pre>) is ${value <= gas_gwei_threshold * 2 / 3 ? 'very low' : 'not high'}.\nMaybe it's time to DeFi or NFTs. 😁👍`);
+        if (twitter.length > 0 || telegram.length > 0) {
+          const { socials } = { ...config };
+          await api.post('', {
+            module: 'broadcast',
+            twitter: {
+              messages: twitter,
+              key: socials?.twitter?.api_key,
+            },
+            telegram: {
+              messages: telegram,
+              key: socials?.telegram?.key,
+              parameters: {
+                web_preview: true,
+              },
+            },
+          }).catch(error => { return { data: { error } }; });
+          alerted = true;
+        }
+      }
+    } catch (error) {}
+  }
+  return alerted;
+};
---
+++ functions/api/services/alerts/index.js
@@ -0,0 +1,1 @@
+// import module for date t
