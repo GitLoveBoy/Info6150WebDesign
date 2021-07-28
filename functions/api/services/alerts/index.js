cdde0418507d2c2afea778251f72a233f5585b06
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-07-28 12:45:35 +0000

    powerpc: Fix bad stack check in exception entry

--- functions/api/services/alerts/index.js
+++ functions/api/services/alerts/index.js
@@ -5,3 +5,5 @@ const exchanges_listing = require('./exchanges-listing');
 const fear_and_greed = require('./fear-and-greed');
 const gas = require('./gas');
 const markets = require('./markets');
+const news = require('./news');
+const whale = require('.
