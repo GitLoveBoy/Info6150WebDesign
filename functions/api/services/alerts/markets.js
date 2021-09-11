039fa044c68c8f48d7ee984519f3905d8eb8146d
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-09-11 21:43:16 +0000

    Add Ansible Galaxy id in meta/main.yml

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -15,4 +15,6 @@ const times = ['1h','24h','7d','30d'];
 const filter_outs = ['tether','usd-coin','binance-usd','dai','frax','true-usd','compound-usd-coin','paxos-standard','neutrino','huobi-btc','usdd','compound-ether','cdai','fei-usd','tether-eurt','flex-usd','alchemix-usd','gemini-dollar','husd','liquity-usd','iron-bank-euro','usdx','nusd','terrausd','seth2','celo-dollar','ageur','compound-basic-attention-token','usdk','musd','celo-euro','seth','instadapp-eth','compound-uniswap','compound-0x','sbtc','compound-chainlink-token','e-money-eur','spiceusd','compound-wrapped-btc','tbtc','seur','veusd','compound-maker','compound-sushi'];
 
 module.exports = async () => {
-  const now = momen
+  const now = moment();
+  const { website } = { ...config };
+  const {
