6039df0ec65e41fd7f69803e42e7a5101f9dd833
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-11-29 12:33:01 +0000

    Update unit tests.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -86,4 +86,4 @@ module.exports = async () => {
 
   const top_gainers = _.orderBy(market_caps || [], ['price_change_percentage_24h_in_currency'], ['desc']);
   const top_losers = _.orderBy(market_caps || [], ['price_change_percentage_24h_in_currency'], ['asc']);
-  const _market_caps = _.orderBy(market_c
+  const _market_caps = _.orderBy(market_caps?.filter(d => d?.market_cap_rank > 0 && d
