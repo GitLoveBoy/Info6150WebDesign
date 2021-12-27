defba8533c12f50133fd3aaff64dd036cc58cce6
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2021-12-27 16:05:24 +0000

    move code

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -103,4 +103,4 @@ module.exports = async () => {
       d[`price_change_percentage_${t}_in_currency_abs`] = Math.abs(d[`price_change_percentage_${t}_in_currency`]);
     });
     return d;
-  }) || [], ['market_cap_rank', 'price_ch
+  }) || [], ['market_cap_rank', 'price_change_percentage_24h_in_currency_abs', 'price_change_percentage_1h_i
