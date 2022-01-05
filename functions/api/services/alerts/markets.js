8b6260c32614557ec5704bd7e6b0ac3a4f12b29e
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-01-05 10:30:18 +0000

    * change writting.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -106,4 +106,7 @@ module.exports = async () => {
   }) || [], ['market_cap_rank', 'price_change_percentage_24h_in_currency_abs', 'price_change_percentage_1h_in_currency_abs'], ['asc', 'desc', 'desc']);
   const _nfts = _.orderBy(nfts?.map(d => {
     times.forEach(t => {
-      d[`price_change_percentage_${t}_in_currency_abs`] = Math.abs(d[`price_change_percentage_${t}_in_curr
+      d[`price_change_percentage_${t}_in_currency_abs`] = Math.abs(d[`price_change_percentage_${t}_in_currency`]);
+    });
+    return d;
+  }) || [], ['market_cap_rank', 'p
