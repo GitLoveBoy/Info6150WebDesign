8f79abe251bdb5d12f910623207f1cc6afb1feef
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-02-11 17:35:41 +0000

    misc regressions

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -117,4 +117,4 @@ module.exports = async () => {
     return d;
   }) || [], ['rank', 'price_change_percentage_24h_in_currency_abs', 'price_change_percentage_1h_in_currency_abs'], ['asc', 'desc', 'desc']);
   const aths = _market_caps.filter(d => now.diff(moment(d?.ath_date), 'hours', true) <= 1);
-  const atls = _m
+  const atls = _market_caps.filter(d => now.diff(moment(d?.atl_date), 'hours'
