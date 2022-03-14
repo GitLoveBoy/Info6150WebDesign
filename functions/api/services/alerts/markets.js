527a7d310a57c3b060f7408f8034b5df6c3fee57
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-03-14 15:43:10 +0000

    Rubber-stamped by Adam Roben.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -140,4 +140,5 @@ module.exports = async () => {
         let twitter_message = '', telegram_message = '';
         data.forEach((d, i) => {
           const { id, symbol, name, value } = { ...d };
-          twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} hits a new ATH at ${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}. 🚀🌙`
+          twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} hits a new ATH at ${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}. 🚀🌙`;
+          telegram_message += `${i === 0 ? '🛸🌙 ALL 
