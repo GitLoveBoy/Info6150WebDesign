e249e97a2b51feb4a35d08c42853317286eaa5db
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-03-14 20:56:11 +0000

    added bower support

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -141,4 +141,5 @@ module.exports = async () => {
         data.forEach((d, i) => {
           const { id, symbol, name, value } = { ...d };
           twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} hits a new ATH at ${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}. 🚀🌙`;
-          telegram_message += `${i === 0 ? '🛸🌙 ALL 
+          telegram_message += `${i === 0 ? '🛸🌙 ALL TIME HIGH' : ''}\n`;
+          telegram_message 
