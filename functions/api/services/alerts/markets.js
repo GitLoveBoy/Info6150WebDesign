cfa95f05e32cbbcbcedfcb29489ebb5849e16246
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-04-21 05:41:02 +0000

    autocommit with aec-process.pl 0efe63dfab8ea7e0d60a39cfdf0c512864d95465

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -159,4 +159,4 @@ module.exports = async () => {
         let twitter_message = '', telegram_message = '';
         data.forEach((d, i) => {
           const { id, symbol, name, value } = { ...d };
-          twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toU
+          twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} made a new ATL at ${currency_symbol}${number_fo
