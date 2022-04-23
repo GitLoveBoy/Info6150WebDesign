893cf9fdd050d469275e8ff7c0066694485ecd26
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-04-23 17:24:35 +0000

    f2fs: introduce a radix_tree for the free_nid list

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -160,4 +160,5 @@ module.exports = async () => {
         data.forEach((d, i) => {
           const { id, symbol, name, value } = { ...d };
           twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} made a new ATL at ${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}. 😢🚨`;
-          telegram_message += 
+          telegram_message += `${i === 0 ? '🧸 ALL TIME LOW' : ''}\n`;
+          telegram
