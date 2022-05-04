9a08bdc0eb8fcac37de81dd5a7cfab75bd930853
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-05-04 11:19:28 +0000

    * Added test code of CSS for h2 tag.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -161,4 +161,4 @@ module.exports = async () => {
           const { id, symbol, name, value } = { ...d };
           twitter_message += `${i > 0 ? '\n' : ''}${symbol ? `$${symbol.toUpperCase()}` : name} made a new ATL at ${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}. 😢🚨`;
           telegram_message += `${i === 0 ? '🧸 ALL TIME LOW' : ''}\n`;
-          telegram_message += `<a href="${website}/token/${id}">${symbol
+          telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <pre>${currency_symbol}$
