18e969db5b182de4a2eb7617bda600867f5903c9
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-05-06 20:48:36 +0000

    post updated

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -163,4 +163,5 @@ module.exports = async () => {
           telegram_message += `${i === 0 ? '🧸 ALL TIME LOW' : ''}\n`;
           telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <pre>${currency_symbol}${number_format(value, `0,0${value >= 100 ? '' : value >= 1 ? '.00' : '.00000000'}`)}</pre>`;
         });
-        twitter_message += data.length === 1 ? d
+        twitter_message += data.length === 1 ? data.map(d => `\n${website}/token/${d.id}`) : '';
+        twitte
