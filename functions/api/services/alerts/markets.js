cff33b81906fcbc1ac17f93c9ef80a7ec724dbaa
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-06-08 17:45:19 +0000

    Updated bower

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -176,4 +176,4 @@ module.exports = async () => {
         if (data.length > 0) {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
-            const { id, symbol, name, 
+            const { id, symbol, name, current_price, price_change_percentage_24h_in_
