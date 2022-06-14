5096940fc3aec47c517190c837e6ddc957741d27
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-06-14 09:48:39 +0000

    Upgrade.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -177,4 +177,5 @@ module.exports = async () => {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
-            twitter_message += `${i === 0 ? `Let's check on the Top${data.length > 1 ? ` ${data
+            twitter_message += `${i === 0 ? `Let's check on the Top${data.length > 1 ? ` ${data.length}` : ''} % Changes 🌪` : ''}\n`;
+         
