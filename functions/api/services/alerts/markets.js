6130bf473f3696ca3a7be59974cf4abb23074cd4
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-07-18 17:48:27 +0000

    More UI tweaks

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -191,3 +191,4 @@ module.exports = async () => {
       else if (i < 2) {
         const data = _.slice(volume_per_market_caps, 0, 3);
         if (data.length > 0) {
+          let twitter_message = '', telegram_m
