1cfb70daa46ce42cb20fdcac2d211d94de4e779d
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-07-18 18:09:40 +0000

    portability

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -191,4 +191,6 @@ module.exports = async () => {
       else if (i < 2) {
         const data = _.slice(volume_per_market_caps, 0, 3);
         if (data.length > 0) {
-          let twitter_message = '', telegram_m
+          let twitter_message = '', telegram_message = '';
+          data.forEach((d, i) => {
+   
