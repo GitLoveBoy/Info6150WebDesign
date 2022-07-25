435e285c160ead4d33f0b0ad213b0b59559d6a8a
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-07-25 07:54:59 +0000

    Bump version: 0.2.1 → 0.2.2

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -197,4 +197,4 @@ module.exports = async () => {
             twitter_message += `${i === 0 ? `Let's check on the Top${data.length > 1 ? ` ${data.length}` : ''} Volume / Market Cap 🌊` : ''}\n`;
             twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)} ${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}`;
             telegram_message += `${i === 0 ? `<a href="${website}/tokens">🌊 High Volume / Market Cap</a>` : ''}\n`;
-            telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <b>${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)}</b> <pre>${numbe
+            telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <b>${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)}</b> <pre>${number_format(price_change_percentage_24h_in_currency
