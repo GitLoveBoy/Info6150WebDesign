cfd1f14543103d7a055c23c0c6913039cdf6f8b6
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-07-26 05:32:57 +0000

    Modify README.md

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -199,4 +199,4 @@ module.exports = async () => {
             telegram_message += `${i === 0 ? `<a href="${website}/tokens">🌊 High Volume / Market Cap</a>` : ''}\n`;
             telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <b>${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)}</b> <pre>${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}</pre>\n<b>Vol/MCap: ${number_format(volume_per_market_cap, '0,0.0000')}</b>`;
           });
-      
+          twitter_message += data.length === 1 ? data.map(d =>
