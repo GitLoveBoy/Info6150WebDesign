55a3abfb6e6003f656785367a3307efa75ed556d
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-05 06:37:35 +0000

    build: Updating minimist to 1.2.5

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -218,4 +218,5 @@ module.exports = async () => {
           });
           twitter_message += data.length === 1 ? data.map(d => `\n${website}/token/${d.id}`) : `\n${website}/tokens`;
           twitter_message += `\n\n💙 if you HODL any one of them\n\n${data.map(d => `${d.name ? `#${d.name.split(' ').filter(c => c).join('')}` : ''}`).join(' ')} `;
-          twi
+          twitter.push(twitter_message);
+          telegram.push(
