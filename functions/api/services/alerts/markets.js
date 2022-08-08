6a09cfab7bccaf68006784b1f5121ff5cb58596d
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-08 10:04:22 +0000

    latest ui

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -229,4 +229,4 @@ module.exports = async () => {
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
             twitter_message += `${i === 0 ? `Today's Top Losers ⚰️` : ''}\n`;
-            twitter_message 
+            twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${curr
