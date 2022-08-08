0bd07e94ddcad4a2dc642fe02f03b0d5b13d12ce
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-08 08:25:27 +0000

    Added via script.

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -228,4 +228,5 @@ module.exports = async () => {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
-            twitter_message += `${i === 0 ? `Tod
+            twitter_message += `${i === 0 ? `Today's Top Losers ⚰️` : ''}\n`;
+            twitter_message 
