44e1aaade6bcbff426eba4e812c0792bd7c05de3
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-08 22:15:48 +0000

    Update heap.c

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -229,4 +229,4 @@ module.exports = async () => {
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
             twitter_message += `${i === 0 ? `Today's Top Losers ⚰️` : ''}\n`;
-            twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_p
+            twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)} $
