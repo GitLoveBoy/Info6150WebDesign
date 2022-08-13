c0e448b3f57b6289f1f749e61bfdb6e0a7f3ac5e
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-13 11:44:50 +0000

    Update Esperanto translation

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -234,4 +234,4 @@ module.exports = async () => {
             telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <b>${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)}</b> <pre>${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}</pre>`;
           });
           twitter_message += data.length === 1 ? data.map(d => `\n${website}/token/${d.id}`) : `\n${website}/tokens`;
-          twitter_message += `\n\n${data.map(d => `${
+          twitter_message += `\n\n${data.map(d => `${d.name ? `#${d.name.split(' ').filter(c => c).join('')}` 
