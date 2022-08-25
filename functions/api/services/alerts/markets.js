3bc4b49c24166f84e72a974d7b35bf17f6576c4f
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-25 22:55:02 +0000

    twiddle

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -263,4 +263,4 @@ module.exports = async () => {
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
             twitter_message += `${i === 0 ? `🎮🏞👻 Update on the Top${data.length > 1 ? ` ${data.length}` : ''} NFTs` : ''}\n`;
-            
+            twitter_message += `${symbol ? `$${symbol.toUppe
