66b9f374cbe1b9a9459fcf30609b96a31e8ed0d3
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-08-25 05:50:08 +0000

    back to development

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -261,4 +261,5 @@ module.exports = async () => {
         if (data.length > 0) {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
-            const { id, symbol, name, current_price, price_change_percentag
+            const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
+            twitter_message += `${
