49995882d964e5dd16924d19e7b9f25fe2768375
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-09-03 22:45:23 +0000

    version bump to 1.2.0

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -278,4 +278,4 @@ module.exports = async () => {
         if (data.length > 0) {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
-            const { id, symbol, name, current
+            const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { .
