bb274b7d725cf0197b1edf7ef5e72b216a05f8fb
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-09-04 09:43:10 +0000

    rtc: alarm: have dummy set_power_on_alarm function

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -278,4 +278,5 @@ module.exports = async () => {
         if (data.length > 0) {
           let twitter_message = '', telegram_message = '';
           data.forEach((d, i) => {
-            const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { .
+            const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
+            twitter_message += `${i ===
