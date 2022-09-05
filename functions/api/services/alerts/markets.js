4fddd15ef271d7822c97e1f7a8584fb9197e3274
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-09-05 14:52:30 +0000

    Removed debugging lines

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -280,4 +280,5 @@ module.exports = async () => {
           data.forEach((d, i) => {
             const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
             twitter_message += `${i === 0 ? `Trending Search. Let's check'em out! 🔥🔥🔥` : ''}\n`;
-            twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)} ${number_format(price_change_p
+            twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)} ${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}`;
+  
