735ad48d4db8a5d6b02de551f0be63e4cf7b91a2
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-09-22 06:07:39 +0000

    Add curl

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -294,4 +294,5 @@ module.exports = async () => {
   }
   else {
     const data = _.slice(market_caps, 0, 3).map(d => {
-      d.hour_market_cap_change = d.price
+      d.hour_market_cap_change = d.price_change_percentage_1h_in_currency * d.market_cap;
+     
