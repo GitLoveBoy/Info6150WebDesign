2d8a4f2b66cfdf056661db46a78184c028c62e6d
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-10-18 19:33:11 +0000

    update checkpoints

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -311,4 +311,4 @@ module.exports = async () => {
           const { id, symbol, name, current_price, price_change_percentage_24h_in_currency } = { ...d };
           twitter_message += `${i === 0 ? `${status === 'panic' ? '😱🥶😰 Some Panic selling detected:' : '🤩🤑😁 Some FOMO buying detected:'}` : ''}\n`;
           twitter_message += `${symbol ? `$${symbol.toUpperCase()}` : name} ${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)} ${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}`;
-          telegram_message += `${i === 0 ? `<a href="${website}">${status === 'panic' ? '😱🥶😰 Pani
+          telegram_message += `${i === 0 ? `<a href="${website}">${status === 'panic' ? '😱🥶😰 Panic Selling' : '🤩🤑😁 FOMO Buying'}</a>` : ''}\n`;
