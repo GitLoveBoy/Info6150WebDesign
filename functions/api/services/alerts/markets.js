5d5f73e46d11dfc86a6b9962024d5312d4c1cadc
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-10-24 05:45:36 +0000

    automatic update of gnatprove_by_example

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -314,4 +314,4 @@ module.exports = async () => {
           telegram_message += `${i === 0 ? `<a href="${website}">${status === 'panic' ? '😱🥶😰 Panic Selling' : '🤩🤑😁 FOMO Buying'}</a>` : ''}\n`;
           telegram_message += `<a href="${website}/token/${id}">${symbol ? symbol.toUpperCase() : name}</a> <b>${currency_symbol}${number_format(current_price, `0,0${current_price >= 100 ? '' : current_price >= 1 ? '.00' : '.00000000'}`)}</b> <pre>${number_format(price_change_percentage_24h_in_currency / 100, '+0,0.00%')}</pre>`;
         });
-        twitter_message += d
+        twitter_message += data.length === 1 ? data.map(d => `\n${website}/token
