5cffed616cfb8164e30f81472ad6644428930f30
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2022-10-26 15:44:52 +0000

    status

--- functions/api/services/alerts/markets.js
+++ functions/api/services/alerts/markets.js
@@ -317,4 +317,9 @@ module.exports = async () => {
         twitter_message += data.length === 1 ? data.map(d => `\n${website}/token/${d.id}`) : `\n${website}/tokens`;
         twitter_message += `\n\n${data.map(d => `${d.name ? `#${d.name.split(' ').filter(c => c).join('')}` : ''}`).join(' ')} `;
         twitter.push(twitter_message);
-        telegram.push(t
+        telegram.push(telegram_message);
+      }
+    }
+  }
+
+  if (twitter.length > 0 || telegram.le
