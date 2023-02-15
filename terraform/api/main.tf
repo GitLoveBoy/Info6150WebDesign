e030f0f88955c1c0622d76a402d3dad0eb6718f1
GitLoveBoy <114841444+GitLoveBoy@users.noreply.github.com>
2023-02-15 15:13:24 +0000

    SCSI: libsas: fix sas_discover_devices return code handling

--- terraform/api/main.tf
+++ terraform/api/main.tf
@@ -32,3 +32,51 @@ data "aws_iam_policy_document" "policy" {
       identifiers = ["lambda.amazonaws.com"]
       type        = "Service"
     }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "${var.package_name}-role-lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_policy_attachment" "attachment" {
+  name       = "${var.package_name}-attachment"
+  roles      = [aws_iam_role.role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_opensearch_domain" "domain" {
+  domain_name     = "${var.package_name}"
+  engine_version  = "OpenSearch_2.3"
+  cluster_config {
+    instance_type            = "t3.small.search"
+    instance_count           = 1
+    dedicated_master_enabled = false
+    zone_awareness_enabled   = false
+    warm_enabled             = false
+  }
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp2"
+    volume_size = 32
+  }
+  encrypt_at_rest {
+    enabled = true
+  }
+  node_to_node_encryption {
+    enabled = true
+  }
+  domain_endpoint_options {
+    enforce_https       = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
+  advanced_security_options {
+    enabled                        = true
+    internal_user_database_enabled = true
+    master_user_options {
+      master_user_name     = var.indexer_username
+      master_user_password = var.indexer_password
+    }
+  }
+}
